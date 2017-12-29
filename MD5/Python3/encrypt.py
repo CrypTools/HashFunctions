0d4ca2c8305a144aef70102148aa43ce8098f35e
Luka Lafaye de Micheaux <31606786+lukalafaye@users.noreply.github.com>
2017-12-29 12:06:50 +1100

    Update encrypt.py

--- MD5/Python3/encrypt.py
+++ MD5/Python3/encrypt.py
@@ -1,6 +1,9 @@
 import hashlib
 
 def encrypt(initial):
+    """ Use : encrypt("message")
+=> '78e731027d8fd50ed642340b7c9a63b3'
+  """
   initial = initial.encode('utf-8')
   output = hashlib.md5(initial.encode())
   return output.hexdigest()
