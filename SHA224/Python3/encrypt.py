b1e92cec46c167fc6e55fa617f48fa4a53d409a1
Luka Lafaye de Micheaux <31606786+lukalafaye@users.noreply.github.com>
2017-12-29 12:08:56 +1100

    Update encrypt.py

--- SHA224/Python3/encrypt.py
+++ SHA224/Python3/encrypt.py
@@ -1,6 +1,9 @@
 import hashlib
 
 def encrypt(initial):
+    """ Use : encrypt("message")
+=> 'ff51ddfabb180148583ba6ac23483acd2d049e7c4fdba6a891419320'
+  """
   initial = initial.encode('utf-8')
   output = hashlib.sha224(initial.encode())
   return output.hexdigest()
