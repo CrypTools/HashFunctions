692346db76f0375f0dde32614ea1137a6e29466c
Luka Lafaye de Micheaux <31606786+lukalafaye@users.noreply.github.com>
2017-12-29 12:07:56 +1100

    Update encrypt.py

--- SHA1/Python3/encrypt.py
+++ SHA1/Python3/encrypt.py
@@ -1,6 +1,9 @@
 import hashlib
 
 def encrypt(initial):
+    """ Use : encrypt("message")
+=> '6f9b9af3cd6e8b8a73c2cdced37fe9f59226e27d'
+    """
   initial = initial.encode('utf-8')
   output = hashlib.sha1(initial.encode())
   return output.hexdigest()
